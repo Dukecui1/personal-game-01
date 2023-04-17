9cb361ee2bfaa2209c9b1138b037e81e458f9d9e
junhai cui <cuijunhai99@126.com>
2023-04-17 17:14:27 -0400

    update
    add JMenuBar
    add one actionMenu
    add connectItem into action

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,39 @@
 package src;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Client extends JFrame implements Runnable {
+
+    public Client() {
+        super("9053 final project");
+        //FRAME Configuration
+        setPreferredSize(new Dimension(1400, 900));
+        setVisible(true);
+        setResizable(false);
+        //COMPONENT Configuration
+        createMenu();
+    }
+
+    private void createMenu() {
+        JMenuBar jMenuBar = new JMenuBar();
+        JMenu actionMenu = new JMenu("Action");
+        JMenuItem connectItem = new JMenuItem("Connect");
+        connectItem.addActionListener(new OpenConnectionListener());
+    }
+    class OpenConnectionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+        }
+    }
 
     @Override
     public void run() {
 
     }
+
 }
